d25217ecaa287b5d0eef3306f902971b03f7b6a8
MatheusMarinhoLeca <matheus.demoraisleca@ucalgary.ca>
2026-09-05 21:33:13 -0600

    Order sections: about, expertise, work

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
       <SiteNav />
       <main id="main">
         <Hero />
-        <Work />
-        <Skills />
         <About />
+        <Skills />
+        <Work />
         <Contact />
       </main>
       <SiteFooter />
